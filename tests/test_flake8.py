6b2c50a69f590ff1824af2f44383213682945b61
Taylor Lyle <lylet@allegheny.edu>
2019-01-18 14:47:51 -0500

    added more to the readme.md

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -1,3 +1,5 @@
+# code was written with help from the gatorgrouper repository
+
 import glob
 import os
 from flake8.api import legacy as flake8
@@ -10,7 +12,7 @@ def test_flake8():
     name_of_files = []
 
     # fill list with all python files found in all subdirectories
-    for root, _, files in os.walk("BlockBuster", topdown=False):
+    for root, _, files in os.walk("Pynesthesia", topdown=False):
         files = glob.glob(root + "/../*.py")
         name_of_files.extend(files)
 
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,5 @@
 import utilities
-import PIL
+from PIL import Image
 import pickle
 import os
 # local imports
@@ -16,23 +16,48 @@ def test_get_color_dict():
     pass
 
 def test_get_unique_color_list():
+    """Gather a list of expected values and compare them against the output of the function"""
 
-    # list may look inflated, but the output map is scaled.
-
+    # 
     expected_list = [(0, 0, 0), (255, 255, 255), (0, 0, 255), (255, 0, 0), (0, 255, 0)]
-
     actual_list = utilities.get_unique_color_list(os.path.join(TEST_FOLDER, "testmap.png"))
 
     assert repr(actual_list) == repr(expected_list)
 
 def test_get_color_map_list():
-    pass
+    """Use the testmap.png to ensure the color map list is correct"""
 
-def test_get_color_map_list():
-    pass
+    image = Image.open("testmap.png")
+    actual_list = utilities.get_color_map_list(image)
+
+    # a representation of the images' colors with RGB tuples
+    expected_list = [
+    [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], # row of black
+    [(0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0)], # row of green
+    [(255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0)], # row of red
+    [(255, 255, 255), (255, 255, 255), (255, 255, 255), (255, 255, 255)] # row of white
+    ]
+
+    assert repr(actual_list) == repr(expected_list)
 
 def test_return_updated_list():
-    pass
+    """Use two lists to test expected output of return_updated_list"""
+    # Expected output is:
+    # 1.) list of unique colors with new colors added
+    # 2.) flag that is true if a new color is in the new list
+    # 3.) list containing only the added colors
+
+    old_list = [(255, 255, 255), (0, 0, 0), (0, 0, 255)]
+    new_list = [(255, 255, 255), (0, 0, 0), (255, 0, 0)]
+
+    updated_list, new_color_flag, new_colors = utilities.return_updated_list(old_list, new_list)
+
+    # should be the same as the old_list with any data appended to it
+    assert repr(updated_list) == [(255, 255, 255), (0, 0, 0), (0, 0, 255), (255, 0, 0)]
+    # new color exists in new_list so this should be true
+    assert repr(new_color_flag) == True
+    # this only contains the elements of new_list that are not in old_list
+    assert repr(new_colors) == [(255, 0, 0)]
 
 def test_write_file():
     pass
